a916135084f59b3f3c5e3791e45b720e0c8cf847
Volkan <vlkn0888@gmail.com>
2021-12-17 13:42:11 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace TwoDimensionalArrays
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(@"D:\01.txt");
+            var jagged = lines.Select(line => line.Split(',').ToArray()).ToArray();
+
+
+            for (var i = 0; i < lines.Length; i++)
+            {
+                for (var j = 0; j < jagged.Length - 1; j++) Console.Write(jagged[i][j] + @" * ");
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
